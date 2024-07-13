a70150a8f18c03a995e4a56ddf156d187932ecda
Wael Karman <wael.karman@gmail.com>
2024-07-13 22:31:22 +0200

    wip: add client server demo implementation

--- client/client-udp.cpp
+++ client/client-udp.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
 
 using namespace std;
 
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+    const char *message = "Hello, Server";
+    socklen_t addr_len = sizeof(server_addr);
 
-int main(){
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        std::cerr << "Errore nella creazione del socket" << std::endl;
+        return 1;
+    }
 
-    cout << "HELLO CLIENT" << endl;   
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    int n = sendto(sockfd, message, strlen(message), 0, (const struct sockaddr *)&server_addr, addr_len);
+    if (n < 0) {
+        std::cerr << "Errore nell'invio dei dati" << std::endl;
+        close(sockfd);
+        return 1;
+    }
+
+    n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&server_addr, &addr_len);
+    if (n < 0) {
+        std::cerr << "Errore nella ricezione dei dati" << std::endl;
+        close(sockfd);
+        return 1;
+    }
+
+    buffer[n] = '\0';
+    std::cout << "Ricevuto dal server: " << buffer << std::endl;
+
+    close(sockfd);
     return 0;
 }
--- server/server-udp.cpp
+++ server/server-udp.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
 
 using namespace std;
 
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[BUFFER_SIZE];
+    socklen_t addr_len = sizeof(client_addr);
 
-int main(){
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        std::cerr << "Errore nella creazione del socket" << std::endl;
+        return 1;
+    }
 
-    cout << "HELLO SERVER" << endl;   
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Errore nel binding del socket" << std::endl;
+        close(sockfd);
+        return 1;
+    }
+
+    std::cout << "Server in ascolto sulla porta " << PORT << std::endl;
+
+    while (true) {
+        int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &addr_len);
+        if (n < 0) {
+            std::cerr << "Errore nella ricezione dei dati" << std::endl;
+            continue;
+        }
+
+        buffer[n] = '\0';
+        std::cout << "Ricevuto: " << buffer << " da " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << std::endl;
+
+        const char *response = "Dati ricevuti";
+        sendto(sockfd, response, strlen(response), 0, (const struct sockaddr *)&client_addr, addr_len);
+    }
+
+    close(sockfd);
     return 0;
 }
