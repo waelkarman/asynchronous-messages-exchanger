49d449c83ad3c5e0f39d3dcc5f228bd1cee7204e
Wael Karman <wael.karman@gmail.com>
2024-07-13 23:09:29 +0200

    wip: add datapacket lib draft

---
+++ libs/datapacket/datapacket.cpp
@@ -0,0 +1,6 @@
+
+#include "datapacket.hpp"
+
+datapacket::datapacket(){
+
+};
---
+++ libs/datapacket/datapacket.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+class datapacket{
+
+public:
+    datapacket();
+
+    string pack(int seq, string data){
+        stringstream ss;
+        ss << seq << ":" << data;
+        return ss.str();
+    }
+
+    std::vector<std::string> unpack(string s){
+        std::vector<std::string> tokens;
+        std::string token;
+        std::stringstream ss(s);
+
+        while (std::getline(ss, token, ':')) {
+            tokens.push_back(token);
+        }
+
+        return tokens;
+    }
+
+private:
+    int seq;
+    string data;
+    string delimiter = ":";
+};
+
+
+// int main(){
+
+//     datapacket d;
+
+//     string s = d.pack(5,"ciao");
+    
+//     std::vector<std::string> v = d.unpack(s);
+//     cout << v[0] << " " << v[1];
+
+// }
